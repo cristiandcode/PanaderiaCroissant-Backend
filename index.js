4f74b0e3c0a226ef846b446cadf7f78e61a017d2
Cristian <diazcristian0210@gmail.com>
2025-10-02 16:24:55 -0300

    Configuré el puerto de la variable de entorno

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log('Hola Mundo')
+import express from 'express';
+
+// 1- Configurar un puerto
+const app = express();
+//crear una variable de entorno
+app.set('port', process.env.PORT || 4000);
+app.listen(app.get('port'), ()=> {
+    console.log('Estoy escuchando el puerto '+ app.get('port'));
+})
+// 2- Configurar los middlewares
+
+// 3- Crear las rutas
+
+//Minuto 42 15 de mayo de 2024 - Veremos que es el framework Express y realizaremos algunas configuraciones basicas en el Backend - Crearemos un nuevo proyecto en MongoDB y lo configuraremos 👨‍💻👩‍💻
