08dd180d6fdf7d526d531dbfe2fce7bed3485d94
Cristian <diazcristian0210@gmail.com>
2025-10-09 18:42:05 -0300

    Cree el schema para dar de alta productos

--- src/controllers/productos.controllers.js
+++ src/controllers/productos.controllers.js
@@ -1,6 +1,13 @@
+import Producto from "../database/models/producto.js"
+
 export const listarProductos = (req, res)=>{
     console.log('Prueba de solicitud GET')
     //Enviar respuesta
    res.send('Desde mi backend de Panaderia Croissant')
+}
+export const crearProducto = (req, res)=>{
+    //Validar los datos del producto del body
+    //Le pedimos a la BD crear el producto
+    //Enviar la respuesta al front
 }
 
---
+++ src/database/models/producto.js
@@ -0,0 +1,45 @@
+import mongoose, {Schema} from "mongoose";
+
+const productoSchema = new Schema({
+    nombreProducto:{type: String, 
+        required: true,
+        minLength: 2,
+        maxLength: 50,
+        unique: true,
+    },
+    precio: {
+        type: Number,
+        required: true,
+        min: 50,
+        max: 20000,
+    },
+    imagen: {
+        type: String,
+        required: true,
+        validate: {
+            validator: (valor) =>{
+                const pattern = /(http(s?):)([/|.|\w|\s|-])*\.(?:jpg|jpeg|gif|png)/
+                return pattern.test(valor)
+            }
+        }
+    },
+    categoria: {
+        type: String,
+        required: true,
+        enum: ['Infusiones', 'Batidos', 'Dulce', 'Salado', 'Sandwich']
+    },
+    descripcion_breve: {
+        type: String,
+        requiered: true,
+        minLength: 5,
+        maxLength: 100,
+    },
+    descripcion_amplia:{
+        type: String,
+        minLength:30,
+        maxLength: 500,
+    }
+});
+
+const Producto = mongoose.model('producto', productoSchema);
+export default Producto;
