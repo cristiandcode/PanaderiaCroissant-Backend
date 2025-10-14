ec7c2fc957b149d164ca5b087da6941e6715f415
Cristian <diazcristian0210@gmail.com>
2025-10-14 15:45:52 -0300

    Agregue la logica de la funcion para obtener un producto y controlamos dos tipos de errores

--- src/controllers/productos.controllers.js
+++ src/controllers/productos.controllers.js
@@ -1,29 +1,48 @@
-import Producto from "../database/models/producto.js"
+import Producto from "../database/models/producto.js";
 
-export const listarProductos = async (req, res)=>{
-    try{
-        //Pedirle a la bd los productos
-        const productos = await Producto.find();
-        //Enviar la respuesta al front con los productos
-        res.status(200).json(productos)
-    }catch(error){
-        console.error(error)
-        res.status(500).json({mensaje: 'Error al listar los productos'})
+export const listarProductos = async (req, res) => {
+  try {
+    //Pedirle a la bd los productos
+    const productos = await Producto.find();
+    //Enviar la respuesta al front con los productos
+    res.status(200).json(productos);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ mensaje: "Error al listar los productos" });
+  }
+};
+
+export const obtenerProducto = async (req, res) => {
+  try {
+    //Extraer el id unico o parametro del request
+    console.log(req.params.id);
+    //Pedimos a la base de datos buscar ese producto
+    const productoBuscado = await Producto.findById(req.params.id);
+    //Quiero saber si productoBuscado es null
+    if(!productoBuscado){
+     return  res.status(400).json({mensaje: `El producto con id: ${req.params.id} no existe`})
     }
-}
-export const crearProducto = async (req, res)=>{
-    try{
+    //Responder al front con el producto buscado
+    res.status(200).json(productoBuscado);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ mensaje: "Error al obtener el producto" });
+  }
+};
+
+export const crearProducto = async (req, res) => {
+  try {
     //Validar los datos del producto del body
     //Le pedimos a la BD crear el producto
-    console.log(req.body)
-    const productoNuevo = new Producto(req.body)
+    console.log(req.body);
+    const productoNuevo = new Producto(req.body);
     await productoNuevo.save(); //Aqui se guarda en la bd
-    //Enviar la respuesta al front  
-        res.status(201).json({mensaje: 'Producto creado con exito', producto: productoNuevo})
-    }catch(error){
-        console.error(error)
-        res.status(500).json({mensaje: 'Error al crear el producto'})
-    }
-
-}
-
+    //Enviar la respuesta al front
+    res
+      .status(201)
+      .json({ mensaje: "Producto creado con exito", producto: productoNuevo });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ mensaje: "Error al crear el producto" });
+  }
+};
--- src/routes/productos.routes.js
+++ src/routes/productos.routes.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { crearProducto, listarProductos } from "../controllers/productos.controllers.js";
+import { crearProducto, listarProductos, obtenerProducto } from "../controllers/productos.controllers.js";
 const router = Router(); 
 
 //crear las rutas
 router.route('/prueba').get(listarProductos)
-router.route('/productos').post(crearProducto).get(listarProductos)
+router.route('/productos').post(crearProducto).get(listarProductos);
+router.route('/productos/:id').get(obtenerProducto)
 export default router;
 
 // 89i 2023 - 22 de mayo de 2024 - Continuamos con el backend - Que es un controlador - Crearemos endpoints para poder obtener, crear y editar productos 👨‍💻👩‍💻 minuto 1:11hs
