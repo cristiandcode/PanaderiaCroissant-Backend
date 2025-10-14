8e7e2f4db7b5be3ed54b623c8abc05f2519c2df9
Cristian <diazcristian0210@gmail.com>
2025-10-14 13:11:03 -0300

    Agregue la logica necesaria para dar de alta productos desde el backend

--- src/controllers/productos.controllers.js
+++ src/controllers/productos.controllers.js
@@ -5,9 +5,19 @@ export const listarProductos = (req, res)=>{
     //Enviar respuesta
    res.send('Desde mi backend de Panaderia Croissant')
 }
-export const crearProducto = (req, res)=>{
+export const crearProducto = async (req, res)=>{
+    try{
     //Validar los datos del producto del body
     //Le pedimos a la BD crear el producto
-    //Enviar la respuesta al front
+    console.log(req.body)
+    const productoNuevo = new Producto(req.body)
+    await productoNuevo.save(); //Aqui se guarda en la bd
+    //Enviar la respuesta al front  
+        res.status(201).json({mensaje: 'Producto creado con exito', producto: productoNuevo})
+    }catch(error){
+        console.error(error)
+        res.status(500).json({mensaje: 'Error al crear el producto'})
+    }
+
 }
 
--- src/routes/productos.routes.js
+++ src/routes/productos.routes.js
@@ -4,5 +4,7 @@ const router = Router();
 
 //crear las rutas
 router.route('/prueba').get(listarProductos)
-router.route('/producto').post(crearProducto)
+router.route('/productos').post(crearProducto)
 export default router;
+
+// 89i 2023 - 22 de mayo de 2024 - Continuamos con el backend - Que es un controlador - Crearemos endpoints para poder obtener, crear y editar productos 👨‍💻👩‍💻 minuto 24
